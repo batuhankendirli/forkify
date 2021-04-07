8ceda3f8c04082675cc6f9f8462346311a8da062
batuhankendirli <batuhankndrl@gmail.com>
2021-04-08 01:01:25 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c6511eba-abe1-42e2-b19a-b9bc9d3d8d4e';
+export const MODAL_CLOSE_SEC = 2.5;
